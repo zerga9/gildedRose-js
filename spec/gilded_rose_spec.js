77ad6805220ae47bfa418f473c06c4add084f5ee
Gadiza Zerari <gadizaz@hotmail.com>
2018-06-19 14:53:50 +0100

    Test quality never negative passing

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,22 @@
 describe("Gilded Rose", function() {
 
+  var gildedRose;
+
+  beforeEach(function() {
+    gildedRose = new Shop();
+    items = gildedRose.updateQuality();
+  });
+
   it("should foo", function() {
     const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toEqual("foo");
+  });
+
+  it("the quality of an item is never negative", function() {
+    var item = new Item("foo", 0, 0);
+    gildedRose.items.push(item);
+    expect(items[0].quality).toBe(0);
   });
 
 });
