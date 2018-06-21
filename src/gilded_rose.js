1b6b20005b6c94c1272a78a0121367e9cc82f48f
Gadiza Zerari <gadizaz@hotmail.com>
2018-06-21 16:16:11 +0100

    agedBrie and Backstage Passes extracted

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -16,26 +16,43 @@ class Shop {
     item = item
   }
 
+  agedBrie(item) {
+    if (item.sellIn > 0){
+      if (item.quality < 50){
+        item.quality += 1;
+      } else if (item.sellIn <= 0){
+        item.quality += 2;
+      }
+    }
+   item.sellIn -= 1;
+ };
+
+  backStagePasses(item) {
+    if(item.sellIn <= 0){
+      item.quality = 0;
+    } else{
+      item.quality += 1
+      if (item.sellIn < 11){
+      item.quality += 1
+    }
+      if (item.sellIn < 6){
+        item.quality += 1;
+      }
+      }
+      if (item.quality >= 50){
+        item.quality = 50
+      }
+    }
+
+
   updateQuality() {
     this.items.forEach((item) => {
       if (item.name == 'Sulfuras, Hand of Ragnaros'){
         this.sulfuras(item);
-      } else if (item.name == 'Aged Brie' || item.name == 'Backstage passes to a TAFKAL80ETC concert'){
-        if (item.quality < 50) {
-              item.quality += 1;
-              item.sellIn -= 1;
-          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.sellIn < 11) {
-              item.quality += 1;
-             }
-             if (item.sellIn < 6) {
-               item.quality += 1;
-              }
-              if(item.sellIn < 0){
-                item.quality = 0
-              }
-          }
-        }
+      } else if (item.name == 'Aged Brie'){
+      this.agedBrie(item);
+    } else if(item.name == 'Backstage passes to a TAFKAL80ETC concert'){
+      this.backStagePasses(item);
       } else if(item.name =="Conjured Mana Cake") {
         item.quality -= 2;
         item.sellIn -= 1;
