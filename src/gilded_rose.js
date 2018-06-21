7f7314e9da8d3fc90391faca2e472c2d3a86719c
Gadiza Zerari <gadizaz@hotmail.com>
2018-06-21 14:39:31 +0100

    refactor the != into ==

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -17,46 +17,42 @@ class Shop {
       if (item.name == 'Sulfuras, Hand of Ragnaros'){
         item.quality == item.quality;
         item.sellIn == item.sellIn;
-      } else if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality > 0) {
-            item.quality -= 1;
-            item.sellIn -= 1;
-        }
-    }else {
+      } else if (item.name == 'Aged Brie' || item.name == 'Backstage passes to a TAFKAL80ETC concert'){
         if (item.quality < 50) {
-          item.quality += 1;
-          item.sellIn -= 1;
+              item.quality += 1;
+              item.sellIn -= 1;
           if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (item.sellIn < 11) {
-                item.quality += 1;
-            }
-            if (item.sellIn < 6) {
-                item.quality += 1;
-            }
+              item.quality += 1;
+             }
+             if (item.sellIn < 6) {
+               item.quality += 1;
+              }
           }
-
         }
-      }
-    
-      if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
+      } else {
+        if (item.quality > 0) {
                 item.quality -= 1;
+                item.sellIn -= 1;
             }
-          } else {
-            item.quality = 0;
-          }
-        } else {
+      }
+      if (item.sellIn < 0) {
+        if (item.name == 'Aged Brie'){
           if (item.quality < 50) {
-            item.quality += 1;
-
+                item.quality += 1;
           }
         }
+        else if(item.name == 'Backstage passes to a TAFKAL80ETC concert'){
+          item.quality = 0
+        } else {
+            if (item.quality > 0) {
+              item.quality -= 1;
+            } else {
+              item.quality = 0;
+            }
+        }
       }
-
     });
-
     return this.items;
   }
 }
